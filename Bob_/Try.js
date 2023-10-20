6bad4eee21f97a6ad595d9048027f5b240b64634
IAmDefinitelyNotARobot <144380886+IAmDefinitelyNotARobot@users.noreply.github.com>
2023-10-20 11:35:03 -0500

    Add files via upload

---
+++ Bob_/Try.js
@@ -0,0 +1,236 @@
+let canvas = document.getElementById("canvas1");
+let painter = canvas.getContext("2d");
+let painterfillStyle= "#ffffff";
+
+
+painter.fillRect( 0, 0, 700, 700);
+painter.fillStyle = "#86592d";
+painter.fillRect( 10, 10, 20, 20);
+
+//Body
+painter.fillStyle = "#86592d";
+painter.fillRect( 20, 20, 20, 20);
+
+painter.fillStyle = "#86592d";
+painter.fillRect( 30, 30, 20, 20);
+
+painter.fillStyle = "#86592d";
+painter.fillRect( 40, 40, 20, 20);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 50, 50, 20, 20);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 50, 60, 20, 20);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 50, 70, 20, 20);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 60, 60, 40, 40);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 70, 100, 30, 30);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 80, 80, 60, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 90, 90, 50, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 100, 70, 20, 60);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 100, 70, 30, 70);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 100, 80, 80, 100);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 120, 100, 90, 100);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 130, 140, 90, 90);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 150, 120, 120, 95);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 160, 120, 200, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 200, 100, 40, 60);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 200, 120, 220, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 100, 90, 90, 40);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 190, 160, 100, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 160, 100, 100, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 150, 100, 100, 100);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 150, 100, 200, 90);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 170, 120, 200, 120);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 170, 120, 300, 100);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 170, 150, 330, 60);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 290, 100, 90, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 380, 97, 110, 70);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 400, 90, 140, 100);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 420, 80, 120, 90);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 500, 70, 50, 70);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 360, 200, 50, 32);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 510, 80, 30, 40);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 520, 70, 50, 80);
+
+painter.fillStyle = "#ffd633";
+painter.fillRect( 540, 60, 40, 40);
+
+//Eyes
+
+
+painter.fillStyle = "#ffffff";       
+painter.fillRect( 160, 150, 25, 25);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 210, 120, 25, 25);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 160, 160, 10, 10);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 212, 130, 10, 10);
+
+//Mouth
+
+painter.fillStyle = "#d51038";
+painter.fillRect( 248, 178, 35, 31);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 200, 180, 15, 15);
+
+painter.fillStyle = "#d51038";
+painter.fillRect( 240, 180, 11, 11);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 220, 180, 33, 32);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 280, 170, 15, 15);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 210, 180, 15, 15);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 278, 180, 15, 15);
+
+painter.fillStyle = "#000000";
+painter.fillRect( 210, 190, 15, 15);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 280, 170, 11, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 270, 174, 11, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 260, 176, 11, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 250, 175, 13, 15);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 238, 178, 13, 15);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 228, 180, 15, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 218, 180, 14, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 210, 180, 11, 11);
+
+painter.fillStyle = "#ffffff";
+painter.fillRect( 200, 180, 11, 11);
+
+painter.fillStyle = "#d51038";
+painter.fillRect( 270, 190, 11, 11);
+
+painter.fillStyle = "#d51038";
+painter.fillRect( 250, 200, 11, 11);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This would make a row of yellow squares along the top:
+//let i = 0
+//let x = 10
+//while(i < 13 ){
+ //  painter.fillRect(x,10,20,20) 
+  // x = x + 30
+   //i = i + 1
+//}
+   
+
+
